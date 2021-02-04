10c512cbd3d5e73e8a219f3ebb77d57225821a7c
SeoyoonPark18 <tjdbs118@gmail.com>
2021-02-04 16:32:06 +0900

    Initial Commit

--- app/src/main/java/com/example/qlogapplication/MainActivity.kt
+++ app/src/main/java/com/example/qlogapplication/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnLogin: Button
     lateinit var btnToRegister: Button
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
